80370744feb99db5b2918f80d15643fb7d84a315
Vang Xiong <vangx709@gmail.com>
2023-02-28 10:48:12 -0900

    render with useState, useReducer, State Immutabe

---
+++ my-app/src/components/UseReducer/UseReducer.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from "react";
+
+function UseReducer() {
+  const initialState = 0;
+
+  const reducer = (state, action) => {
+    switch (action) {
+      case "increment":
+        return state + 1;
+
+      case "decrement":
+        return state + 1;
+
+      case "reset":
+        return (state = initialState);
+      default:
+        return state;
+    }
+  };
+
+  const [count, dispatch] = useReducer(reducer, initialState);
+  console.log("UseReducer Render");
+  return (
+    <div>
+      {count}
+      <button onClick={() => dispatch("increment")}>increment</button>
+      <button onClick={() => dispatch("decrement")}>decrement</button>
+      <button onClick={() => dispatch("reset")}>reset</button>
+    </div>
+  );
+}
+
+export default UseReducer;
